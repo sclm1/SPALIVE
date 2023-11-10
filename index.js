ba489efe8ec1fb413a48fba75ff6df55b84cafce
sclm1 <142795026+sclm1@users.noreply.github.com>
2023-11-10 13:44:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 bot.command('start', async (ctx) => {
     const chatId = ctx.message.chat.id
     if (chatId != "-1001959268889") {
-      ctx.replyWithPhoto("https://ibb.co/ZB8bkxW", {caption: `<i><b>Để sử dụng được công cụ - Bạn làm theo các bước sau đây!</b></i>
+      ctx.replyWithPhoto("https://i.ibb.co/WkyRYfg/Screenshot-20231110-113859-org-telegram-messenger.png", {caption: `<i><b>Để sử dụng được công cụ - Bạn làm theo các bước sau đây!</b></i>
       \n<b>Bước 1:</b> Tìm đến trang sản phẩm bạn muốn truy vấn.
       \n<b>Bước 2:</b> Nhấn nút chia sẻ sản phẩm (như hình) và copy link chia sẻ sản phẩm.
       \n<b>Bước 3:</b> Tham gia group https://t.me/CoNenChotDon và paste link sản phẩm vô Giỏ Live, rồi ấn Gửi.
